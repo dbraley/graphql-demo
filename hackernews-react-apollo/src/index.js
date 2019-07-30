41aecfe4a402fc80e9d3440792979fc4507bf686
David Braley <david.braley@gmail.com>
2019-07-29 20:58:50 -0400

    Authentication

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -4,26 +4,39 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
+import { AUTH_TOKEN } from "./constants";
 
-// 1
+// 1 Apollo imports
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
 
 
-// 2
+// 2 Backend GraphQL server
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 });
 
-// 3
+// 2.5  AuthContext
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN);
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ''
+        }
+    }
+});
+
+// 3 Load Apollo Client
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 });
 
-// 4
+// 4 Render page
 ReactDOM.render(
     <BrowserRouter>
         <ApolloProvider client={client}>
