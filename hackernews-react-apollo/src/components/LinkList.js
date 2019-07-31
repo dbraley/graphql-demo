731083716c3321d48270398861618fc99b26c50e
David Braley <david.braley@gmail.com>
2019-07-30 21:32:44 -0400

    More Mutations and Updating the store

--- hackernews-react-apollo/src/components/LinkList.js
+++ hackernews-react-apollo/src/components/LinkList.js
@@ -3,17 +3,29 @@ import Link from './Link'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
     {
         feed {
             links {
                 id
                 url
                 description
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
             }
         }
     }
 `
+
+
 class LinkList extends Component {
     render() {
         return (
@@ -26,12 +38,28 @@ class LinkList extends Component {
 
                     return (
                         <div>
-                            {linksToRender.map(link => <Link key={link.id} link={link} />)}
+                            {linksToRender.map((link, index) => (
+                                <Link
+                                    key={link.id}
+                                    link={link}
+                                    index={index}
+                                    updateStoreAfterVote={this._updateCacheAfterVote}
+                                />
+                            ))}
                         </div>
                     )
                 }}
             </Query>
         )
     }
+    _updateCacheAfterVote = (store, createVote, linkId) => {
+        const data = store.readQuery({ query: FEED_QUERY })
+
+        const votedLink = data.feed.links.find(link => link.id === linkId)
+        votedLink.votes = createVote.link.votes
+
+        store.writeQuery({ query: FEED_QUERY, data })
+    }
+
 }
 export default LinkList
