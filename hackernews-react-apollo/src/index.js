d18dc57595bc06499d5a74fb4a84144a84db6217
David Braley <david.braley@gmail.com>
2019-08-01 21:00:49 -0400

    Realtime Updates with GraphQL Subscriptions

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -5,13 +5,16 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
 import { AUTH_TOKEN } from "./constants";
-
 // 1 Apollo imports
+
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 
 // 2 Backend GraphQL server
@@ -31,8 +34,27 @@ const authLink = setContext((_, { headers }) => {
 });
 
 // 3 Load Apollo Client
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000`,
+    options: {
+        reconnect: true,
+        connectionParams: {
+            authToken: localStorage.getItem(AUTH_TOKEN),
+        }
+    }
+})
+
+const link = split(
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query)
+        return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    wsLink,
+    authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link,
     cache: new InMemoryCache()
 });
 
